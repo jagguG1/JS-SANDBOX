4ddbe0d028eabd6c43ba857500d7a399c3772db2
jagguG1 <jagadeeshreddyl123>
2023-11-27 19:35:49 +0530

    kko

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log('Hello world');
+
+console.log(23,'Hello world');
+
+console.error('Alert');
+
+console.table({ name:'Jaggu', email:'jaggu@123'});
+
+console.group('simple');
+console.log('Hello world');
+console.log(23,'Hello world');
+console.error('Alert');
+console.groupEnd();
+console.log('Hello Alakuntla vasu')
+
+
+
+
+
+
+
